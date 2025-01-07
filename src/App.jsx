7ad96c32e77f7147ac8022925ab5d5f1a7e38460
WillDinauer <77174175+WillDinauer@users.noreply.github.com>
2025-01-07 11:28:50 -0500

    Spotify Auth Redirect Initial Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const geenrateChallenge = async () => {
-    const codeVerifier = generateRandomString(64);
+  const generateChallenge = async (codeVerifier) => {
     const hashed = await sha256(codeVerifier);
     const codeChallenge = base64encode(hashed);
+    return codeChallenge;
+  }
+
+  const authorize = () => {
+    const codeVerifier = generateRandomString(64);
+    const codeChallenge = generateChallenge(codeVerifier);
+
+    const clientId = '18f14aa15de04555bb6265565305ef67';
+    const redirectUri = 'http://localhost:3000';
+
+    const scope = 'user-read-private user-read-email';
+    const authUrl = new URL("https://accounts.spotify.com/authorize")
+    
+    window.localStorage.setItem('code_verifier', codeVerifier);
+
+    const params =  {
+      response_type: 'code',
+      client_id: clientId,
+      scope,
+      code_challenge_method: 'S256',
+      code_challenge: codeChallenge,
+      redirect_uri: redirectUri,
+    }
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
   }
 
   return (
@@ -25,8 +49,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={authorize}>
+          Spotify Auth Test
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
