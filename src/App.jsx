28c6e38079e9c6983cc98b85bee594ff6e6dd6dd
WillDinauer <william.dinauer@gmail.com>
2025-01-06 23:28:03 -0500

    initial setup code for spotify and code challenge generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { generateRandomString, sha256, base64encode } from './spotify'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const geenrateChallenge = async () => {
+    const codeVerifier = generateRandomString(64);
+    const hashed = await sha256(codeVerifier);
+    const codeChallenge = base64encode(hashed);
+  }
 
   return (
     <>
